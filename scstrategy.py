f8f71e63396ff44a80a740adb29a55089feaea04
bulutyelken <190101052@ogrenci.yalova.edu.tr>
2022-12-19 22:26:33 +0300

    bug

--- scstrategy.py
+++ scstrategy.py
@@ -3,62 +3,63 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import mplfinance as mpf
 
-COMMISSION = 0.001
-LIQ_MEMORY = 100
+# COMMISSION = 0.001
+LIQ_MEMORY = 60
+# in_position = "null"
 
-in_position = "null"
-
-budget = 1000
-coin_amount = 0
+# budget = 1000
+# coin_amount = 0
 
 df = pd.read_csv("BTC-2021min-reversed.csv", index_col=2, parse_dates=True)
-
-
 
 liquidities = []
 counter = 0
 
-mins = 0
-hours = 0.0
-days = 0.0
-liqLevel = 0.0
-
-
+liqCandle = []
+dusuyor = False
 for x in range(10, len(df), 1):
-    tdf = df[x:x+100]
+
+    tdf = df[x:x+LIQ_MEMORY]
     tdf.index.name = 'date'
 
     candle = df[x:x+1]
     prev_candle = df[x+1:x+2]
-    mins += 1
+
     lowVal = float(candle['low'])
     prevLowVal = float(prev_candle['low'])
+
     if lowVal < prevLowVal:
-        counter = 0
+        # if lowVal < float(liqCandle.values[5]):  -- ...onun yerine buradaki gibi prevCandle yerine liqCandle'a bakmamiz lazim!
+        #     counter = 0  -- SORUN BURADA MUHTEMELEN, candle bir önceki candledan düsükse direkt counter'i 0'lıyor. bunun fixlenmesi lazim...
         liquidities.append(np.nan)
         if len(liquidities) > LIQ_MEMORY:
             liquidities.pop(0)
-
+        dusuyor = True
     elif prevLowVal < lowVal:
         if counter == 1:
-            liqLevel = lowVal
+            liqCandle = candle
+        dusuyor = False
         counter += 1
         liquidities.append(np.nan)
         if len(liquidities) > LIQ_MEMORY:
             liquidities.pop(0)
 
-    if counter == 5:
-        liquidities.append(liqLevel)
+    if counter == 3:
+        for y in range(5):
+            liquidities.pop(-1)
+
+        liquidities.append(df.iloc[x-4]['low']-20)
         # liquidities.append(tdf.iloc[x]['low'])
         print(liquidities)
         counter = 0
+        liqCandle = []
         if len(liquidities) > LIQ_MEMORY:
             liquidities.pop(0)
-        hours = mins / 60
-        days = hours / 24
-        buy_markers = mpf.make_addplot(liquidities, type='scatter', markersize=120, marker='^')
-        apds = [buy_markers]
-        if len(liquidities) == len(tdf):
-            mpf.plot(tdf, type='candle', tight_layout=True, datetime_format='%b %d, %H:%M', addplot=apds)
 
-    print(len(liquidities))
+    if len(liquidities) == len(tdf) and len(liquidities) == LIQ_MEMORY and x % LIQ_MEMORY == 0:
+        buy_markers = mpf.make_addplot(liquidities, type='scatter', markersize=60, marker='^')
+        apds = [buy_markers]
+        try:
+            mpf.plot(tdf, type='candle', tight_layout=True, datetime_format='%b %d, %H:%M', addplot=buy_markers)
+        except ValueError:
+            pass
