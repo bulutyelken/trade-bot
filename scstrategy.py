1569d8f5be71aa93fab3198b855865bac531c136
bulutyelken <190101052@ogrenci.yalova.edu.tr>
2022-12-27 16:58:29 +0300

    liq sweeping

--- scstrategy.py
+++ scstrategy.py
@@ -4,48 +4,43 @@ import matplotlib.pyplot as plt
 import mplfinance as mpf
 
 # COMMISSION = 0.001
-LIQ_MEMORY = 120
-DEPTH = 5
+LIQ_MEMORY = 100
+DEPTH = 6
 # in_position = "null"
-
 # budget = 1000
 # coin_amount = 0
 
-df = pd.read_csv("BTC-2021min-reversed.csv", index_col=2, parse_dates=True)
+df = pd.read_csv("BTC-2021min-reversed.csv", index_col=2, parse_dates=True)#.iloc[90000:]
 
 liquidities = []
-counter = 0
 liqCandle = []
-dusuyor = False
-liqAlindi = False
+counter = 0
+dususMumlari = [np.nan]
 
 for x in range(0, len(df), 1):
-    tdf = df[x-LIQ_MEMORY:x]
+    tdf = df[x - LIQ_MEMORY:x]
     tdf.index.name = 'date'
 
-    candle = df[x:x+1]
-    prev_candle = df[x+1:x+2]
+    candle = df[x:x + 1]
+    prev_candle = df[x + 1:x + 2]
 
     lowVal = float(candle['low'])
     prevLowVal = float(prev_candle['low'])
-    print(candle.to_numpy()[0][5], " ", counter)
+
     if len(liqCandle) == 0:
         liqCandle = candle.to_numpy()
 
     if lowVal < prevLowVal:
-        dusuyor = True
         if lowVal <= liqCandle[0][5]:
             counter = 0
             liqCandle = candle.to_numpy()
         elif lowVal > liqCandle[0][5]:
             counter += 1
-
         liquidities.append(np.nan)
         if len(liquidities) > LIQ_MEMORY:
             liquidities.pop(0)
 
     elif prevLowVal <= lowVal:
-        dusuyor = False
         if counter == 0:
             liqCandle = prev_candle.to_numpy()
             # print(liqCandle,"len(liqCandle): ",len(liqCandle), "   ", liqCandle[0][5])
@@ -55,15 +50,31 @@ for x in range(0, len(df), 1):
             liquidities.pop(0)
 
     if counter == DEPTH:
+        for i in range(len(liquidities)-1):
+            if len(liquidities) >= LIQ_MEMORY:
+                if liquidities[i] > df.iloc[x - DEPTH]['low']:
+                    liquidities[i] = np.nan
+                    print(len(liquidities), liquidities)
         liquidities[-DEPTH] = df.iloc[x - DEPTH]['low'] - 5
+
         # print(df.iloc[x-DEPTH])
         counter = 0
         liqCandle = candle.to_numpy()
+    #
+    # if float(candle['open']) > float(candle['close']):
+    #     dususMumlari.append(float(candle['low']))
+    # elif float(candle['open']) <= float(candle['close']):
+    #     dususMumlari.append(np.nan)
+    # if len(dususMumlari) > LIQ_MEMORY:
+    #     dususMumlari.pop(0)
+
+    # ------------------------------------------------------------------------------------------,
 
     if len(liquidities) == len(tdf) and len(liquidities) == LIQ_MEMORY and x % 10 == 0:
-        buy_markers = mpf.make_addplot(liquidities, type='scatter', markersize=60, marker='^')
+        buy_markers = mpf.make_addplot(liquidities, type='scatter', markersize=30, marker='^', color='r')
         try:
-            mpf.plot(tdf, figratio=(42,24), type='candle', tight_layout=True, datetime_format='%b %d, %H:%M', addplot=buy_markers)
-        except ValueError:
-            print(ValueError)
+            mpf.plot(tdf, figratio=(16, 9), figscale=1.2, type='candle', tight_layout=True, datetime_format='%b %d, %H:%M', addplot=buy_markers)
+        except ValueError as e:
+            print(e)
             pass
+    # print(float(candle['open']), "-", float(candle['close']), " =  ", float(candle['open']) - float(candle['close']))
